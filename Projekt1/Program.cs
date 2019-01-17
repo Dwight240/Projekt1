8a2d5f5b188f2626d1626ad1f5628846a7af594a
denis007 <denis007@poczta.onet.eu>
2019-01-17 10:43:00 +0100

    Zmiana 3

--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -13,6 +13,28 @@ namespace Projekt1
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!!!!!");
+
+            var a = 1;
+            var b = 1;
+            var c = -6;
+
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            if(delta>0)
+            {
+                var x1 = (-b-Math.Sqrt(delta)) / (2*a);
+                var x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine($"There are two solutions: {x1} and  {x2}.");
+            }
+            else if(delta == 0)
+            {
+                var x = -b / (2 * a);
+                Console.WriteLine($"There is one solution {x}");
+            }
+            else
+            {
+                Console.WriteLine("There is no solution...");
+            }
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
